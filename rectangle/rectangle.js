b6b9f458e8c02c5a43ec90d7d598638b202b8ae6
niuyuxin <1120733074@qq.com>
2020-02-21 08:03:43 +0800

    course

---
+++ rectangle/rectangle.js
@@ -0,0 +1,17 @@
+$(function(){
+  //get dom elem
+  var $width=$('#width'),
+      $height=$('#height'),
+      $btuCal=$('#calculate'),
+      $perimeter=$('#perimeter'),
+      $area= $('#area');
+  /*calc button click event */
+$btuCal.click(function(){
+    //get value
+  
+    //calculate
+  
+    //output
+});
+
+});
