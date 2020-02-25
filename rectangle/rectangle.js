0a80862ace56612187b539eca460cf86aad06a6b
niuyuxin <1120733074@qq.com>
2020-02-25 17:03:51 +0800

    change

--- rectangle/rectangle.js
+++ rectangle/rectangle.js
@@ -8,10 +8,17 @@ $(function(){
   /*calc button click event */
 $btuCal.click(function(){
     //get value
-  
+    var w=Number($width.val());
+    var h=Number($height.val());
     //calculate
-  
+    var p =retain(2*(w+h),2);
+    var a =retain(w*h,2);
     //output
+    $perimeter.val(p);
+    $area.val(a);
 });
+function retain(s,n){
+  return Math.round(s * Math.pow(10, n)) / Math.pow(10, n);
+}
 
 });
